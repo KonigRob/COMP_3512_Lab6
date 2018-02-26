10f2a3a95b10be76b31b02c411f0a76bca9f2b00
Rob Koenig <rkoenig5@my.bcit.ca>
2018-02-26 14:41:52 -0800

    Added select_arents  Need to ask about the pointer stuff

--- COMP_3512_Lab6/Tour.cpp
+++ COMP_3512_Lab6/Tour.cpp
@@ -16,7 +16,7 @@ double Tour::get_tour_distance(Tour)
 	return 0.0;
 }
 
-int Tour::determine_fitness(Tour, int)
+int Tour::determine_fitness(Tour*, int)
 {
 	return 0;
 }
@@ -25,9 +25,22 @@ void Tour::swap_cities(int, int, City)
 {
 }
 
-Tour Tour::select_parents(Tour)
+Tour* Tour::select_parents(Tour* population)
 {
-	return Tour();
+	Tour* parents = new Tour[NUMBER_OF_PARENTS];
+	Tour* parent_pool = new Tour[PARENT_POOL_SIZE];
+	int parent_index = 0;
+
+	for (int i = 0; i < NUMBER_OF_PARENTS; ++i) {
+		for (int j = 0; j < PARENT_POOL_SIZE; ++j) {
+			int k = rand() % POPULATION_SIZE;
+			*(parent_pool + j) = *(population + k);
+		}
+		parent_index = determine_fitness(parent_pool, PARENT_POOL_SIZE);
+		*(parents + i) = *(parent_pool + parent_index);
+	}
+	delete parent_pool;
+	return parents;
 }
 
 Tour Tour::crossover(Tour)
@@ -46,12 +59,12 @@ void Tour::mutate(Tour population)
 
 }
 
-int Tour::contains_city(Tour* candidate_tour, int length, City candidate_city)
+int Tour::contains_city(Tour* candidate_tour, int length, City* candidate_city)
 {
 	for (int i = 0; i < length; ++i) {
-		if (candidate_tour->permutation[i].getname() == (char)candidate_city.getname() &&
-			candidate_tour->permutation[i].getxCoord() == (int)candidate_city.getxCoord() &&
-			candidate_tour->permutation[i].getyCoord() == (int)candidate_city.getyCoord()) {
+		if (candidate_tour->permutation[i].getname() == (char)candidate_city->getname() &&
+			candidate_tour->permutation[i].getxCoord() == (int)candidate_city->getxCoord() &&
+			candidate_tour->permutation[i].getyCoord() == (int)candidate_city->getyCoord()) {
 			return 1;
 		}
 	}
--- COMP_3512_Lab6/Tour.hpp
+++ COMP_3512_Lab6/Tour.hpp
@@ -3,12 +3,15 @@
 #include <iostream>
 #include <ctime>
 #include <random>
+#include <cstdlib>
 #include "City.hpp"
 
 class Tour : public City{
 	static constexpr int CITIES_IN_TOUR = 20;
 	static constexpr int POPULATION_SIZE = 30;
 	static constexpr int NUMBER_OF_ELITES = 1;
+	static constexpr int NUMBER_OF_PARENTS = 2;
+	static constexpr int PARENT_POOL_SIZE = 5;
 	double fitness;
 	City permutation[CITIES_IN_TOUR];
 
@@ -16,11 +19,11 @@ public:
 	void shuffle_cities(City);
 	double get_distance_between_cities(City, City);
 	double get_tour_distance(Tour);
-	int determine_fitness(Tour, int);
+	int determine_fitness(Tour*, int);
 	void swap_cities(int, int , City);
-	Tour select_parents(Tour);
+	Tour* select_parents(Tour*);
 	Tour crossover(Tour);
 	void mutate(Tour);
-	int contains_city(Tour*, int, City);
+	int contains_city(Tour*, int, City*);
 	double random(double, double);
 };
